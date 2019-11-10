f9b9127eac5b65eafe0f2a0e4fed7f62ba0d643c
tsudhishnair <sudhishnair53@gmail.com>
2019-11-10 11:24:56 +0530

    toggle todo implemented

--- src/Components/ListTodo.js
+++ src/Components/ListTodo.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
+import { toggleTodo } from "../Action/actions";
+import $ from "jquery";
+import { nominalTypeHack } from "prop-types";
 class ListTodo extends React.Component {
+  handleClick = (event, id) => {
+    $(event.target).css("text-decoration", "line-through");
+
+    this.props.toggleTodoItem(id);
+  };
   render() {
     return (
       <div>
         Nothing to return as of now
         {/* {console.log(this.props.todolist.byIds)} */}
         {Object.entries(this.props.todolist.byIds).map((item, index) => {
-          return <li key={index}>{item[1].content}</li>;
+          // if (item[1].completed)
+          //   return (
+          //     <li key={index} style={{ textDecoration: "line-through " }}>
+          //       {item[1].content}
+          //     </li>
+          //   );
+          // else
+          return (
+            <li key={index} onClick={e => this.handleClick(e, index + 1)}>
+              {item[1].content}
+            </li>
+          );
         })}
       </div>
     );
@@ -18,7 +37,14 @@ const mapStatetoProps = state => {
     todolist: state.todoReducer
   };
 };
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleTodoItem: id => {
+      dispatch(toggleTodo(id));
+    }
+  };
+};
 export default connect(
   mapStatetoProps,
-  null
+  mapDispatchToProps
 )(ListTodo);
